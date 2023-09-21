8f52e81465992622b6855a11479b576f2906dabe
khanhcy <quockhanhcy@gmail.com>
2023-09-22 00:21:00 +0700

    Revert "test"
    This reverts commit 6e5e1a2814a0cffd5202cc02fd30cb6702e5d75b.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.oop_2023 {
+module com.ui {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
@@ -11,6 +11,6 @@ module com.example.oop_2023 {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    opens com.example.oop_2023 to javafx.fxml;
-    exports com.example.oop_2023;
+    opens com.ui to javafx.fxml;
+    exports com.ui;
 }
