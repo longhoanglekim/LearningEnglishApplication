82d53a4b0b94ff4e55170e69f8a4b9e3ef0b2037
lyhduc <lyhongduc.34@gmail.com>
2023-09-28 21:42:39 +0700

    Changing ui interface
    Add stylesheet and some ui dependence files

---
+++ src/main/java/com/ui/DictionaryApplication.java
@@ -0,0 +1,32 @@
+package com.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+import java.io.IOException;
+
+public class DictionaryApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(DictionaryApplication.class.getResource("Application.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        //Set icon
+        Image icon = new Image("file:src/main/resources/com/ui/zictionary.png");
+        stage.getIcons().add(icon);
+        //Stage settings
+        stage.setTitle("Dictionary Application Demo");
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/ui/SceneController.java
@@ -0,0 +1,17 @@
+package com.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SceneController {
+    private int currentScene = 0;
+    @FXML
+    private Label welcomeText;
+
+   /* @FXML
+    protected void onDictionaryButtonClick() {
+
+
+    }*/
+
+}
