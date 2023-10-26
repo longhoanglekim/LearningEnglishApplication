afd22601b42db8c965e89468a4c3124f0bef8e5e
khanhcy <quockhanhcy@gmail.com>
2023-10-26 23:09:27 +0700

    add translator and TranslateController
    may quá k lỗi :+1:

--- src/main/java/com/controller/TranslateController.java
+++ src/main/java/com/controller/TranslateController.java
@@ -1,14 +1,36 @@
 package com.controller;
 
+import com.ui.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
+import java.awt.*;
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static com.dictionary.Translator.translateEnToVi;
+
+
 public class TranslateController implements Initializable {
+    @FXML
+    public TextField fromField;
+    @FXML
+    public TextField toField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void onTranslateClicked() {
+        try {
+            toField.setText(translateEnToVi(fromField.getText()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
