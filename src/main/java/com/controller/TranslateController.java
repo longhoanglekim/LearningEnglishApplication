3514c5f88ebdaea225de07725432caf4ee7a7506
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2023-11-03 19:31:46 +0700

    Fix line break translate issue.

--- src/main/java/com/controller/TranslateController.java
+++ src/main/java/com/controller/TranslateController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,10 +22,10 @@ public class TranslateController implements Initializable {
     public TextArea fromField;
     @FXML
     public TextArea toField;
-/*    @FXML
-    public ImageView imageFromLanguage;
-    @FXML
-    public ImageView imageToLanguage;*/
+    /*    @FXML
+        public ImageView imageFromLanguage;
+        @FXML
+        public ImageView imageToLanguage;*/
     @FXML
     public Label labelFromLanguage;
     @FXML
@@ -43,13 +45,28 @@ public class TranslateController implements Initializable {
         labelFromLanguage.styleProperty().bind(FontSizeManager.getInstance().fontSizeProperty().asString("-fx-font-size: %dpx;"));;
         labelToLanguage.styleProperty().bind(FontSizeManager.getInstance().fontSizeProperty().asString("-fx-font-size: %dpx;"));
 
-        fromField.setOnKeyPressed(keyEvent -> {
+        fromField.addEventFilter(KeyEvent.KEY_RELEASED, keyEvent -> {
+            String res = fromField.getText();
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                res += "";
+                // Xử lý Enter ở đây nếu cần
+                keyEvent.consume(); // Ngăn Enter lan ra
+
+            }
             try {
-                String res = fromField.getText();
-                res.replace("\\r\\n", "<code>0</code>");
+                res = fromField.getText();
+                String[] tmp = res.split("\n");
+//                for (String str : tmp) {
+//                    System.out.println(str);
+//                }
+                System.out.println("-----------");
+                //res = res.replace("\r\n", "<code>0</code>");
                 String toTrans = "";
                 if (enToVi && !res.isEmpty()) {
-                    toTrans = translateEnToVi(res);
+                    for (String str : tmp) {
+                        toTrans += translateEnToVi(str) + "\n";
+                    }
+
                 } else if (!enToVi && !res.isEmpty()) {
                     toTrans = translateViToEn(res);
                 }
@@ -57,7 +74,9 @@ public class TranslateController implements Initializable {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
         });
+
     }
 
     public void onSwichClicked() {
