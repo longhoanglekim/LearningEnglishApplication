fcaed4188acbc067a57dd9c27577e79eb073369e
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2023-11-13 23:34:22 +0700

    update draw method.

--- src/main/java/com/controller/GameController.java
+++ src/main/java/com/controller/GameController.java
@@ -2,6 +2,10 @@ package com.controller;
 
 import com.dictionary.Dictionary;
 import com.dictionary.Local;
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,6 +21,9 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
 
 import static com.ui.Model.dictionary;
 import java.io.File;
@@ -29,9 +36,13 @@ public class GameController implements Initializable {
     private static final String HANGMAN_PATH = "src/main/resources/data/hangman.txt";
 
     @FXML
+    Pane hangmanDraw;
+    private double posX = 40;
+    private double posY = 200;
+
+    @FXML
     GridPane gridPaneConsonants;
     File file;
-    private String currentGuess;
     private int currentWrongTime = 0;
 
     public String answer;
@@ -91,10 +102,28 @@ public class GameController implements Initializable {
         Button clickedButton = (Button) event.getSource();
 
         System.out.println(clickedButton.getId() + " được nhấn");
-        clickedButton.setVisible(false);
+
         // Thêm mã xử lý tùy ý khi button được nhấn
         // ...
-        handleClickEvent(clickedButton);
+        // Tạo đối tượng FadeTransition
+        FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(1000), clickedButton);
+
+        // Thiết lập giá trị từ alpha (độ trong suốt) 1 (hiện tại) đến 0 (mất đi)
+        fadeOutTransition.setFromValue(1.0);
+        fadeOutTransition.setToValue(0.0);
+
+        // Xử lý sự kiện khi hoàn thành hiệu ứng
+        fadeOutTransition.setOnFinished(e -> {
+            // Thêm mã xử lý tùy ý khi button được nhấn
+            // ...
+            handleClickEvent(clickedButton);
+            clickedButton.setVisible(false);
+
+        });
+
+        // Bắt đầu hiệu ứng
+        fadeOutTransition.play();
+
     }
 
     /**
@@ -108,11 +137,86 @@ public class GameController implements Initializable {
         //System.out.println(buttonID);
         for (int i = 0; i < answer.length(); i++) {
             char tmp = buttonID.charAt(0);
-            if (Character.compare(answer.charAt(i), tmp) == 0) {
-                listLabel.get(i).setText(buttonID.toUpperCase());
-            } else {
-                // Draw hangman
+            boolean find = false;
+            for (int j = i + 1; j < answer.length(); j++) {
+                if (answer.charAt(i) == tmp) {
+                    listLabel.get(i).setText(buttonID.toUpperCase());
+                    find = true;
+                }
             }
+            if (!find) {
+                // Draw hangman
+                currentWrongTime++;
+                draw();
+                break;
+            }
+        }
+    }
+
+    /**
+     * method to draw hangman when getting a wrong guess.
+     */
+    public void draw() {
+
+        if (currentWrongTime == 1) {
+            // Draw vertical
+            Line lineV = new Line(posX, posY, posX, posY);
+            hangmanDraw.getChildren().add(lineV);
+            // Tạo Timeline với hiệu ứng xuất hiện từng đoạn trong 3 giây
+            Duration durationV = Duration.seconds(2);
+            Timeline timelineV = new Timeline();
+
+                // Tạo các KeyFrame và KeyValue
+            for (double t = 0; t <= 1.0; t += 0.01) {
+                KeyValue keyValue = new KeyValue(lineV.endYProperty(), posY - t * 150);
+                KeyFrame keyFrame = new KeyFrame(durationV.multiply(t), keyValue);
+                timelineV.getKeyFrames().add(keyFrame);
+            }
+
+
+            // Chạy Timeline và thiết lập sự kiện hoàn thành
+            timelineV.setOnFinished(event -> {
+                // Draw horizontal
+                Line lineH = new Line(posX, posY - 150, posX, posY - 150);
+                hangmanDraw.getChildren().add(lineH);
+
+                // Tạo Timeline với hiệu ứng xuất hiện từng đoạn trong 3 giây
+                Duration durationH = Duration.seconds(2);
+                Timeline timelineH = new Timeline();
+
+                // Tạo các KeyFrame và KeyValue
+                for (double t = 0; t <= 1.0; t += 0.01) {
+                    KeyValue keyValue = new KeyValue(lineH.endXProperty(), posX + t * 100);
+                    KeyFrame keyFrame = new KeyFrame(durationH.multiply(t), keyValue);
+                    timelineH.getKeyFrames().add(keyFrame);
+                }
+
+                // Chạy Timeline ngang
+                timelineH.play();
+            });
+
+            // Chạy Timeline dọc
+            timelineV.play();
+
+        } else if (currentWrongTime == 2) {
+            Line lineV2 = new Line(posX, posY, posX, posY);
+            hangmanDraw.getChildren().add(lineV2);
+            // Tạo Timeline với hiệu ứng xuất hiện từng đoạn trong 3 giây
+            Duration durationV2 = Duration.seconds(2);
+            Timeline timelineV2 = new Timeline();
+
+            // Tạo các KeyFrame và KeyValue
+            for (double t = 0; t <= 1.0; t += 0.01) {
+                // Thay đổi cách tính toán posY để đảm bảo tính nhất quán
+                double updatedPosY = posY - 150 + t * 50;
+                KeyValue keyValue = new KeyValue(lineV2.endYProperty(), updatedPosY);
+                KeyFrame keyFrame = new KeyFrame(durationV2.multiply(t), keyValue);
+                timelineV2.getKeyFrames().add(keyFrame);
+            }
+
+            // Chạy Timeline dọc
+            timelineV2.play();
+            System.out.println(2);
         }
     }
 }
