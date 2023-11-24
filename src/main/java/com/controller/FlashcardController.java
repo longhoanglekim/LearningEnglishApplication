71af675306c65be3442db668803d51fe3dc5a0b6
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2023-11-24 10:46:37 +0700

    update flashcard view.

--- src/main/java/com/controller/FlashcardController.java
+++ src/main/java/com/controller/FlashcardController.java
@@ -2,9 +2,7 @@ package com.controller;
 
 import com.dictionary.Word;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import static com.ui.Model.dictionary;
@@ -13,33 +11,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlashcardController implements Initializable {
-    public Label questionLabel;
-    public Label answerLabel;
-    public Pane cardPane;
+
     public Button leftButton;
     public Button rightButton;
-    public TextField currentCardField;
     static int currentCard = 0;
+    public Label currentCardLabel;
+    public Pane cardPane;
+    public TextArea answerArea;
+    public TextArea questionArea;
     List<String> cardAnwserList = new ArrayList<>();
     List<String> cardQuestionList = new ArrayList<>();
 
     @Override
     public void initialize(java.net.URL url, java.util.ResourceBundle resourceBundle) {
         loadCard();
-        questionLabel.setText(cardQuestionList.get(currentCard));
-        answerLabel.setText(cardAnwserList.get(currentCard));
-        questionLabel.setVisible(true);
-        answerLabel.setVisible(false);
-        cardPane.setOnMouseClicked(this::flipCard);
-        currentCardField.setText("1/" + cardQuestionList.size());
+        questionArea.setText(cardQuestionList.get(currentCard));
+        answerArea.setText(cardAnwserList.get(currentCard));
+        questionArea.setVisible(true);
+        answerArea.setVisible(false);
+        currentCardLabel.setText((currentCard + 1) + "/" + cardQuestionList.size());
         leftButton.setOnMouseClicked(this::onLeftButton);
         rightButton.setOnMouseClicked(this::onRightButton);
+        cardPane.setOnMouseClicked(this::flipCard);
     }
 
     private void onRightButton(MouseEvent mouseEvent) {
         if (currentCard < cardAnwserList.size() - 1) {
             currentCard++;
-            currentCardField.setText((currentCard + 1) + "/" + cardQuestionList.size());
+            currentCardLabel.setText((currentCard + 1) + "/" + cardQuestionList.size());
         }
         System.out.println(currentCard + 1);
         showQuestion();
@@ -48,14 +47,14 @@ public class FlashcardController implements Initializable {
     private void onLeftButton(MouseEvent mouseEvent) {
         if (currentCard >= 1) {
             currentCard--;
-            currentCardField.setText((currentCard + 1) + "/" + cardQuestionList.size());
+            currentCardLabel.setText((currentCard + 1) + "/" + cardQuestionList.size());
         }
         System.out.println(currentCard + 1);
         showQuestion();
     }
 
     public void flipCard(MouseEvent mouseEvent) {
-        if (questionLabel.isVisible()) {
+        if (questionArea.isVisible()) {
             showAnswer();
         } else {
             showQuestion();
@@ -63,32 +62,25 @@ public class FlashcardController implements Initializable {
     }
 
     public void loadCard() {
-//        cardAnwserList.add("answer 1");
-//        cardAnwserList.add("answer 2");
-//        cardAnwserList.add("answer 3");
-//        cardAnwserList.add("answer 4");
-//        cardQuestionList.add("question 1");
-//        cardQuestionList.add("question 2");
-//        cardQuestionList.add("question 3");
-//        cardQuestionList.add("question 4");
         for (int i = 0; i < dictionary.getBookmarkList().getList().size(); i++) {
             cardQuestionList.add(dictionary.getBookmarkList().getList().get(i));
             Word word = dictionary.lookup(cardQuestionList.get(i));
             if (word != null) {
-                cardAnwserList.add(word.getExplain());
+                cardAnwserList.add(word.getDefinition().toString());
             }
         }
     }
 
     private void showAnswer() {
-        questionLabel.setVisible(false);
-        answerLabel.setVisible(true);
-        answerLabel.setText(cardAnwserList.get(currentCard));
+        questionArea.setVisible(false);
+        answerArea.setVisible(true);
+        answerArea.setText(cardAnwserList.get(currentCard));
     }
 
     private void showQuestion() {
-        questionLabel.setVisible(true);
-        answerLabel.setVisible(false);
-        questionLabel.setText(cardQuestionList.get(currentCard));
+
+        questionArea.setVisible(true);
+        answerArea.setVisible(false);
+        questionArea.setText(cardQuestionList.get(currentCard));
     }
 }
